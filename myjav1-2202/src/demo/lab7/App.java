37f6310aac7a230c1c7589bdae3dbc084411f60d
kimlee <kimlee@10.111.69.27>
2022-03-29 13:50:51 +0800

    jav1-update.demo/tut7

---
+++ myjav1-2202/src/demo/lab7/App.java
@@ -0,0 +1,49 @@
+package demo.lab7;
+
+import java.util.Scanner;
+
+
+public class App {
+
+	static Scanner scan = new Scanner( System.in );
+	
+	public static void doTask( int option ) {
+		switch ( option ) {
+			case 0: 
+				Demo.show(); break;
+			case 1: 
+//				Q1.show(); break;
+			case 2:
+//				Q2.show(); break;
+			case 3:
+//				Q3.show(); break;
+			default:
+				throw new IllegalArgumentException("Unexpected value: " +  option );
+		}
+	}
+	
+    public static void main(String[] args) {
+    	int op = -1;
+    	do {
+    		menu();
+    		System.out.println( ">> Enter your choice: " );
+    		op = scan.nextInt();
+    		
+    		if( op >= 0 ) doTask( op );
+    		
+		} while( op >= 0 );
+    	System.out.println( "\n>>> Program quit!" );
+    	System.exit(0);
+	}
+    
+    static void menu() {
+    	System.out.println( ">>> MENU OPTION..." );
+    	System.out.println( "-1) Quit" );
+    	System.out.println( "0) Demo" );
+    	System.out.println( "1) Question 1" );
+    	System.out.println( "2) Question 2" );
+    	System.out.println( "3) Question 3" );
+    	System.out.println();
+    }
+	
+}
---
+++ myjav1-2202/src/demo/lab7/Demo.java
@@ -0,0 +1,51 @@
+package demo.lab7;
+
+import java.util.Random;
+
+public class Demo {
+	
+	static int[] arr = new int[100];
+	
+    public static void show() {
+
+    	a();
+    	
+    	System.out.println();
+    	
+    	b();
+    	
+		System.out.println("\nDone...");
+		System.out.println("====================================");
+    }
+    
+    static void a()
+    {
+    	
+    	for (int i = 0; i < arr.length; i++) {
+			arr[i] = Math.abs(new Random().nextInt());
+		}
+    	System.out.println( "> Array initialised randomly!" );
+    	
+    }
+    
+    static void b()
+    {
+    	int index = 101; //101 is out of range of the array's capacity 
+    	
+    	//Causing ArrayIndexOutOfBounds exception - unhandled!
+//    	for (int i = 0; i < index; i++) 
+//    	{
+//			System.out.println( arr[i] );
+//		}
+    	
+    	//To handle it!
+    	try {
+        	for (int i = 0; i < index; i++) {
+    			System.out.println( arr[i] );
+    		}	
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.err.println( "> Out of bound!" );
+		}
+    }
+    
+}
