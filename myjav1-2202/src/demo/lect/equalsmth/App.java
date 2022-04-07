b4603ca1ab07682018103a4177cadb0587a2ac36
kimlee <kimlee@10.111.69.106>
2022-04-07 15:10:37 +0800

    jav1-update.lect/equals()

---
+++ myjav1-2202/src/demo/lect/equalsmth/App.java
@@ -0,0 +1,24 @@
+package demo.lect.equalsmth;
+
+public class App {
+
+	
+	/**
+	 * Builtin class Integer
+	 */
+
+	public static void main(String[] args) {
+		
+		Integer i1 = 10; //auto-casting
+		Integer i2 = 10; //auto-casting
+		
+//		System.out.println( i1 == i2 ); //verify the Object reference
+//		System.out.println( i1.equals(i2) ); //verify the value of the objects
+		
+		
+		System.out.println( "hello".hashCode() );
+
+		
+	}
+	
+}
