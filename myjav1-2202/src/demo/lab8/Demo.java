e18dbfd3bc527188d21c8c62ef6fcda5769dd91a
kimlee <kimlee@10.111.69.111>
2022-04-12 16:05:06 +0800

    jav1-update.lab8/demo

--- myjav1-2202/src/demo/lab8/Demo.java
+++ myjav1-2202/src/demo/lab8/Demo.java
@@ -1,6 +1,13 @@
 package demo.lab8;
 
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
 
 public class Demo {
 
@@ -16,8 +23,32 @@ public class Demo {
     {
     	JFrame window1 = new JFrame();
     	window1.setTitle("Window 1");
-    	window1.setSize(300, 200);
+    	window1.setSize(400, 200);
+    	window1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    	window1.setLocationRelativeTo(null);
+    	window1.setLayout(new FlowLayout());
+    	
+    	/**
+    	 * Add input controls
+    	 */
+    	JTextField tf1 = new JTextField(10);
+    	JTextField tf2 = new JTextField(10);
+    	window1.add(tf1); window1.add(tf2);
+    	JButton btEnter = new JButton("Enter");
+    	window1.add(btEnter);
+    	btEnter.addActionListener( new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(null, "Entering...");
+			}
+    		
+    	} );
+    	
+    	
+    	
     	window1.setVisible(true);
+    	
     }
     
     static void window2()
