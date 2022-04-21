394d998126f7abdf66d7bcda61b8f56d4c31c88f
kimlee <kimlee@10.111.69.57>
2022-04-21 16:55:05 +0800

    jav1-update.demo/lab9

---
+++ myjav1-2202/src/demo/lab9/Demo.java
@@ -0,0 +1,48 @@
+package demo.lab9;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class Demo {
+    
+    public static void show() {
+        
+    	List<String> nameLst = new ArrayList<>();
+
+    	System.out.println( ">> Reading is in progress..." );
+    	
+    	try {
+    		TimeUnit.SECONDS.sleep(3);
+			List<String> memberLst = Files.readAllLines( Paths.get("src/demo/lab9/memberLst.txt") );
+			if ( memberLst.size() > 0 ) {
+				for (String line : memberLst) {
+					String[] token = line.split("\\,");
+					nameLst.add( token[1] );
+				}
+			}
+			
+			System.out.println( "--- Member list ---" );
+	    	disp(nameLst);
+	    	
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    	
+    	System.out.println("\nDone...");
+		System.out.println("====================================");
+    }
+    
+    static void disp( List<String> lst ) throws Exception
+    {
+    	System.out.println( "Total: "+ lst.size() );
+    	for (String s: lst) {
+    		TimeUnit.SECONDS.sleep(1);
+			System.out.println( "> "+s );
+		}
+    }
+    
+}
